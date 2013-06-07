590493598c645c3e675321e5a32697c70c33c4f6
markymark <smallitle@hotmail.com>
2013-06-07 11:38:40 -0700

    added subject again

---
+++ components/subject.h
@@ -0,0 +1,142 @@
+/* 
+om636
+Copyright (c) 2013 by Mark Busenitz
+www.github.com/mucbuc
+
+objective: 
+    collection of subject policies
+
+*/ 
+
+#ifndef SUBJECT_H__3OinTKlusSf57wk9G41geIdYXzUEVv
+#define SUBJECT_H__3OinTKlusSf57wk9G41geIdYXzUEVv
+
+#include <om636/components/observer.h>
+#include <om636/components/context.h>
+#include <om636/core/persistent.h>
+#include <om636/create/singleton.h>
+
+#include <vector> 
+#include <map>
+
+namespace om636
+{
+    // no attach/detach while update
+    template<class T>
+    struct basic_subject
+    : default_subject::policy< T >
+    {
+        typedef T context_type;
+        typedef typename default_subject::traits<context_type>::value_type value_type;
+        typedef observer< context_type > * observer_type;
+
+        basic_subject();
+        virtual ~basic_subject();
+        void attach(const observer_type &);
+		void detach(const observer_type &);
+        virtual void on_swap(context_type &, context_type &);
+        
+        template<class V>
+        static value_type on_init(V &);
+        
+        template<class V, class W>
+        static value_type on_init(V &, const W &);
+        
+        template<class V>
+        static V to_value(const context_type &);
+
+    protected:
+        
+        typedef std::vector< observer_type > container_type;
+        typedef typename container_type::iterator iterator;
+        
+        container_type m_observers;
+    };
+    
+    template<class T>
+    struct safe_subject
+    : public T
+    {
+        struct locked
+        {};
+        
+        typedef T base_type;
+        using typename base_type::context_type;
+        using typename base_type::observer_type;
+        
+        safe_subject();
+        virtual ~safe_subject();
+        
+        void attach(const observer_type &);
+        void detach(const observer_type &);
+        virtual void on_swap(context_type &, context_type &);
+        
+    private:
+        bool m_locked;
+    };
+    
+    template<class T>
+    struct persistent_subject
+    : public T
+    {
+        typedef T base_type;
+        using typename base_type::context_type;
+        using typename base_type::value_type;
+        using base_type::on_init;
+
+        persistent_subject();
+        persistent_subject(const char *);
+        virtual ~persistent_subject();
+        
+        template<class U>
+        static value_type on_init(U &, const std::string &);
+    
+        virtual void on_swap(context_type &, context_type &);
+        
+        std::string & name();
+        std::string name() const;
+        
+    private:
+        
+        typedef om636::persistent< std::map< std::string, std::string > > persistent_type;
+        typedef singleton< persistent_type, default_lifetime< persistent_type >, create_new< persistent_type > > singleton_type;
+        
+        std::string m_name;
+    };
+    
+    template<class T>
+	struct state_subject
+    : private T
+    {
+        // types
+        typedef T policy_type;
+        
+        typedef typename policy_type::value_type value_type;
+        typedef typename policy_type::context_type context_type;
+        using policy_type::on_init;
+        using policy_type::to_value;
+        using policy_type::state;
+        
+        virtual ~state_subject();
+        
+        // comparisons
+		virtual bool on_equal(const context_type &, const context_type &) const;
+		virtual int on_cmp(const context_type &, const context_type &) const;
+		virtual int on_sign(const context_type &) const;
+        
+		// modifiers
+        virtual bool on_swap(context_type &, context_type &) const;
+		virtual void on_add(context_type &, const context_type &) const;
+		virtual void on_subtract(context_type &, const context_type &) const;
+		virtual void on_mult(context_type &, const context_type &) const;
+		virtual void on_divide(context_type &, const context_type &) const;
+		virtual void on_remainder(context_type &, const context_type &) const;
+        virtual void on_inc(context_type &) const;
+        virtual void on_dec(context_type &) const;
+        virtual void on_invert(context_type &) const;
+    };
+    
+}	// om636
+
+#include "subject.hxx"
+#endif // SUBJECT_H__3OinTKlusSf57wk9G41geIdYXzUEVv
