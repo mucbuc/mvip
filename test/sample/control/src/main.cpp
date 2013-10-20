d825b39d8a3bcd42d7be296c89d9a52744ce371b
markymark <smallitle@hotmail.com>
2013-10-20 14:58:40 -0700

    cleanup

--- test/sample/control/src/main.cpp
+++ test/sample/control/src/main.cpp
@@ -6,40 +6,40 @@
 #include <iostream>
 
 
-struct test_base
-{
-    virtual ~test_base() {
-        destructor_called = true;
-    }
-    
-    static bool destructor_called;
-};
-
-bool test_base::destructor_called( false );
-
 template<typename T>
 struct test_emitter_policy
 {
+    struct test_base
+    {
+        virtual ~test_base() {
+            destructor_called = true;
+        }
+        
+        static bool destructor_called;
+    };
+    
     typedef test_base object_type;
 };
 
-void nop() {}
+template<typename T>
+bool test_emitter_policy<T>::test_base::destructor_called( false );
 
 void check_destructor() {
 
     using namespace std;
     using namespace om636;
-    typedef emitter< string, function<void()> , test_emitter_policy > emitter_type;
+    typedef function<void()> function_type;
+    typedef emitter< string, function_type , test_emitter_policy > emitter_type;
     typedef typename emitter_type::Listener listener_type;
     
     emitter_type e;
     
     {
-        listener_type l( e.on( string(""), nop ));
+        listener_type l( e.on( string(""), function_type() ));
     }
     
-    assert( test_base::destructor_called );
-    cout << "destructor test passed" << endl;
+    assert( listener_type::destructor_called );
+    cout << "emitter: destructor test passed" << endl;
 }
 
 int main() {
