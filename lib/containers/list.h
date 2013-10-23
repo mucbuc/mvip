9afb4e423baa34540e45fc7c9064aa95f491face
markymark <smallitle@hotmail.com>
2013-10-22 17:22:29 -0700

    removed toolbag

--- lib/containers/list.h
+++
@@ -1,7 +0,0 @@
-#ifndef LIST_H__j2KWHZsRzCUVkmwQJgNFuY7MEei14p
-#define LIST_H__j2KWHZsRzCUVkmwQJgNFuY7MEei14p
-
-//source code
-
-#include "list.hxx"
-#endif // __LIST_H__j2KWHZsRzCUVkmwQJgNFuY7MEei14p
