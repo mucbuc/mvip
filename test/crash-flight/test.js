8fcba7d91852f2deca2688814e7a3d9a02c85e0e
markymark <smallitle@hotmail.com>
2013-10-22 23:14:44 -0700

    synchronous test

--- test/crash-flight/test.js
+++ test/crash-flight/test.js
@@ -2,13 +2,23 @@
 
 var fs = require( 'fs' )
   , join = require( 'path' ).join
-  , exec = require( 'mucbuc-jsthree' ).exec
+  , Processor = require( 'mucbuc-jsthree' ).Processor
   , runDir = __dirname + (process.argv[2] ? process.argv[2] : '/../sample')
-  , MAKE_PATH = './../../crash-flight/config/makefile';
+  , MAKE_PATH = './../../crash-flight/config/makefile'
+  , EventEmitter = require( 'events' ).EventEmitter;
   
-function execute( cmd, cwd, done ) {
-  console.log( ':node test$ ' + cmd ); 
-  return exec( cmd, cwd, done );
+function execute( cmd, args, cwd, done ) {
+  var e = new EventEmitter()
+    , p = new Processor( { cmd: cmd, args: args, cwd: cwd }, e );
+  console.log( ':node test$ ' + cmd + ' ' + args.toString() + ' ' + cwd );
+  e.on( 'child_error', function() { 
+    console.log( 'error' );
+  } );
+  e.on( 'read', function( data ) {
+    console.log( data.toString() );
+  } );
+  e.once( 'exit', done );
+  e.emit( 'execute' );
 }
 
 (function() {
@@ -20,19 +30,34 @@ function execute( cmd, cwd, done ) {
   
   fs.readdir( runDir, function( err, list ) {
     if (err) throw err;
-    list.forEach( function( file ) {
-      var path = join( runDir, file ); 
+    
+    nextFile();
 
-      fs.stat( path, function( err, stat ) {
-        if (stat && stat.isDirectory()) {
-        
-          console.log( 'testing ' + path + ':' );
+    function nextFile() {
+      if (list.length)  {
+        compileAndRun( list[0] );
+        list = list.slice( 1, list.length );
+      }
   
-          execute( 'make -f ' + MAKE_PATH + ' run', function() {
-            execute( 'make -f ' + MAKE_PATH + ' clean', function() {}, path );
-          }, path );
-        }
-      } );
-    } );
+      function compileAndRun( file ) {
+
+        var path = join( runDir, file ); 
+
+        fs.stat( path, function( err, stat ) {
+          if (stat && stat.isDirectory()) {
+          
+            console.log( 'testing ' + path + ':' );
+    
+            execute( 'make', [ '-f', MAKE_PATH, 'run' ], path, function() {
+              execute( 'make', [ '-f', MAKE_PATH, 'clean' ], path, nextFile );
+            } );
+          } 
+          else {
+            nextFile();
+          }
+        } );
+      }
+    }
+
   } );
 })();
