a4a028c1d90481da9f49399947de03d6ac9589e3
markymark <smallitle@hotmail.com>
2014-02-02 10:31:08 -0800

    file stubs

---
+++ src/presenter.h
@@ -0,0 +1,7 @@
+#ifndef PRESENTER_H__3lGF6XdngzyLbrkU7mQVA9ai5u1Tqj
+#define PRESENTER_H__3lGF6XdngzyLbrkU7mQVA9ai5u1Tqj
+
+//source code
+
+#include "presenter.hxx"
+#endif // PRESENTER_H__3lGF6XdngzyLbrkU7mQVA9ai5u1Tqj
---
+++ src/view.h
@@ -0,0 +1,7 @@
+#ifndef VIEW_H__0qpIVawxhQjit7UC6ES3oWvKnMAJOf
+#define VIEW_H__0qpIVawxhQjit7UC6ES3oWvKnMAJOf
+
+//source code
+
+#include "view.hxx"
+#endif // VIEW_H__0qpIVawxhQjit7UC6ES3oWvKnMAJOf
